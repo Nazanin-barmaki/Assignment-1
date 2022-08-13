b04187a80145a8c197f5c0e1267d67f64701d2dd
Nznin <110465626+Nazanin-barmki@users.noreply.github.com>
2022-08-13 20:11:40 +0430

    Add files via upload

---
+++ masahat mosalasl.py
@@ -0,0 +1,5 @@
+ghaede=int(input("enter ghaede:"))
+ertefae=int(input("enter ertefae:"))
+masahat=(ghaede*ertefae)/2
+print("masahat mosalas:",masahat)
+
---
+++ ostovane.py
@@ -0,0 +1,9 @@
+height=int(input("enter height:"))
+pi=5
+shoae=int(input("enter shoae:"))
+sath=2(pi*shoae*height)+2(pi*shoae*shoae)
+masahat=2(pi*shoae*height)
+hajm=pi*shoae*shoae*height
+print("masahat ostovane:",masahat)
+print("hajm ostovane:",sath)
+print("sath koli",sath)
