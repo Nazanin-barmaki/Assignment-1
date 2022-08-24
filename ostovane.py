653d184064c01fc3fe15f8a007cad28abd7f477c
Nznin <110465626+Nazanin-barmaki@users.noreply.github.com>
2022-08-24 20:52:05 +0430

    Update ostovane.py

--- ostovane.py
+++ ostovane.py
@@ -1,9 +1,9 @@
-height=int(input("enter height:"))
-pi=5
-shoae=int(input("enter shoae:"))
+height=float(input("enter height:"))
+shoae=float(input("enter shoae:"))
+pi=3.14
 sath=2(pi*shoae*height)+2(pi*shoae*shoae)
 masahat=2(pi*shoae*height)
 hajm=pi*shoae*shoae*height
-print("masahat ostovane:",masahat)
-print("hajm ostovane:",sath)
-print("sath koli",sath)
+print("masahat ostovane:","masahat")
+print("hajm ostovane:","sath")
+print("sath koli","sath")
